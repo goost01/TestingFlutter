1935289bd1cab4582727b78076d45c5f05755a35
goost01 <nicolas.silvaf@sansano.usm.cl>
2022-08-24 18:50:28 -0400

    commit-6

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -62,10 +62,20 @@ class _LoginViewState extends State<LoginView> {
                 email: email,
                 password: password
                 );
-                Navigator.of(context).pushNamedAndRemoveUntil(
-                  mainRoute, 
-                  (route) => false
+                final user = FirebaseAuth.instance.currentUser;
+                if (user?.emailVerified ?? false) {
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    mainRoute, 
+                    (route) => false
                 );
+                }else {
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    verifyEmailRoute, 
+                    (route) => false
+                );
+
+                }
+                
               } on FirebaseAuthException catch(e){
                 devtools.log(e.toString());
                 if (e.code == 'user-not-found'){
